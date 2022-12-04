0185d7baa006c30d16b78e5ee051f0f2849a1e95
Kunal <kunalsonawane730@gmail.com>
2022-12-04 10:53:23 +0530

    adding test case to throw empty or null mood custom exception

--- src/test/java/com/datastructure/MoodAnalyzerTest.java
+++ src/test/java/com/datastructure/MoodAnalyzerTest.java
@@ -4,23 +4,53 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyzerTest {
-    @Test
-    public void testMoodAnalysis_whenMoodIsSad() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a sad message");//Tc1.1 Refactor
-        String mood = moodAnalyzer.analyseMood();
-        Assert.assertEquals(mood,"SAD");
-    }
-
-    @Test
-    public void testMoodAnalysis_whenMoodIsHappy() {//Tc1.2 Refactor
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a happy message");
-        String mood1 = moodAnalyzer.analyseMood();
-        Assert.assertEquals(mood1,"HAPPY");
-    }
-    @Test
-    public void testMoodAnalysis_whenMoodIsNull() { //tc 2.1
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
-        String mood2 = moodAnalyzer.analyseMood();
-        Assert.assertEquals(mood2,"HAPPY");
+         //  Analyse mood by passing the message in the constructor
+         @Test
+         public void testMoodAnalysis_whenMoodIsSad() {//uc1 refactor
+             MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a sad message");
+             String mood;
+             try {
+                 mood = moodAnalyzer.analyseMood();
+                 Assert.assertEquals("SAD" , mood);
+             } catch (MoodAnalysisException e) {
+                 e.printStackTrace();
+             }
+         }
+         //Analyse mood by passing the message in the constructor
+         @Test
+         public void testMoodAnalysis_whenMoodIsHappy() {//uc1refactor
+             MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a happy message");
+             String mood;
+             try {
+                 mood = moodAnalyzer.analyseMood();
+                 Assert.assertEquals("HAPPY" , mood);
+             }catch (MoodAnalysisException e) {
+                 e.printStackTrace();
+             }
+         }
+     
+      // Custom Exception if User Provides Invalid Mood
+          // Input message: null
+         @Test
+         public void testMoodAnalysis_whenMoodIsNull_ShouldThrowException() {//uc 3.1
+             MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+             try {
+                 moodAnalyzer.analyseMood();
+             } catch (MoodAnalysisException e) {
+                 Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_NULL, e.type);
+             }
+         }
+         // Custom Exception if User Provides Invalid Mood
+          //Input message: empty
+         @Test
+         public void testMoodAnalysis_whenMoodIsEmpty_ShouldThrowException() {//uc3.2
+             MoodAnalyzer moodAnalyzer = new MoodAnalyzer("");
+             try {
+                 moodAnalyzer.analyseMood();
+             } catch (MoodAnalysisException e) {
+                 System.out.println(e);
+                 System.out.println(e.type);
+                 Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_EMPTY , e.type);
+             }
     }
 }
