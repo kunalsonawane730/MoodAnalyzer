230fcadbd906f9b37df3f38da6e319ff3949816d
Kunal <kunalsonawane730@gmail.com>
2022-12-04 10:26:14 +0530

    adding program to handle null exception

--- src/main/java/com/datastructure/MoodAnalyzer.java
+++ src/main/java/com/datastructure/MoodAnalyzer.java
@@ -2,20 +2,23 @@ package com.datastructure;
 
 public class MoodAnalyzer {
     private String message;
-
     //Default Constructor
     public MoodAnalyzer() {
     }
-
     //Parameterized Constructor
     public MoodAnalyzer(String message) {
         this.message = message;
     }
 
+    //Handle NULLPOINTER Exception using try-catch block
     public String analyseMood() {
-        if (message.contains("sad"))
-            return "SAD";
-        else
+        try {
+            if (message.contains("sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch(Exception e) {
             return "HAPPY";
+        }
     }
 }
